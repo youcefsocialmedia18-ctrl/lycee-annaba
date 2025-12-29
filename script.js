94552468349d6f121a64722a5e7c62b768707884
youcefsocialmedia18-ctrl <youcefsocialmedia18@gmail.com>
2025-12-29 15:42:05 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// التمرير بين القسمين فقط
 const sections = document.querySelectorAll('.hero, .content');
 let currentSection = 0;
 let scrolling = false;
@@ -8,9 +7,9 @@ window.addEventListener('wheel', (e) => {
     scrolling = true;
 
     if (e.deltaY > 0 && currentSection < sections.length - 1) {
-        currentSection++; // النزول للقسم الثاني
+        currentSection++; 
     } else if (e.deltaY < 0 && currentSection > 0) {
-        currentSection--; // الصعود للقسم الأول
+        currentSection--; 
     }
 
     sections[currentSection].scrollIntoView({
@@ -20,12 +19,12 @@ window.addEventListener('wheel', (e) => {
     setTimeout(() => scrolling = false, 800);
 }, { passive: false });
 
-// أنيميشن البطاقات عند الاقتراب من الشاشة
+
 window.addEventListener("scroll", () => {
     document.querySelectorAll(".card").forEach(card => {
         let position = card.getBoundingClientRect().top;
         if (position < window.innerHeight - 100) {
-            card.classList.add("visible"); // اضف class visible بدلاً من تعديل transform مباشرة
+            card.classList.add("visible");
         }
     });
 });
